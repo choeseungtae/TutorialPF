e5c4dc83fd11cba4860cbeffeded6896f22cb2c0
choeseungtae <choeseungtae99@gmail.com>
2025-03-13 17:25:19 +0900

    0313 게임 UI, UX

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -4,19 +4,14 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
+    public GameObject ClearInfoObject;
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("클리어했습니다.");  
+        ClearInfoObject.SetActive(true);
 
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        Debug.Log("충돌 지점에서 벗어났어요.");
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        Debug.Log("충돌 중입니다.");
-    }
+    
 }
